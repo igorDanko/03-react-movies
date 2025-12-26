f9b5634aa738725e1668b71b795c7011d4b1f028
igor <igordanko2222@gmail.com>
2025-12-26 12:12:27 +0200

    add homework

---
+++ src/vite-env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TMDB_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
